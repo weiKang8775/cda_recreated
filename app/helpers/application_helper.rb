59821d5b7e5e34541d0a96d9bb2993c1a122b394
Wei Kang <wei.kang8775@gmail.com>
2020-09-21 13:27:55 -0600

    Add more stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     html_options[:class] = "#{html_options[:class]} active".strip if current_page?(options)
     link_to(name, options, html_options)
   end
+
+  def get_title
+    if current_page?(root_path)
+      return "Free Ultimate Guide to CDA Interviews: Tips & Proven Strategies to Help You Prepare & Ace Your CDA Structured Interview"
+    elsif current_page?(contact_us_path)
+      return "Contact Us"
+    end
+  end
 end
